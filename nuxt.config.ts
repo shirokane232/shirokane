2a1166aa1957df2a69700f1dea80178bbd08eb13
shirokane232 <shirokane232@gmail.com>
2025-07-31 00:43:59 +0800

    update base url for gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  app: {
+    baseURL: '/shirokane/',
+    buildAssetsDir: 'assets',
+  },
 });
